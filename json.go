3405daccd924942a40d39543278c0a73c3d48d22
liuyangsds <262565914@qq.com>
2021-10-06 07:55:50 +0800

    v=1.1.8

--- json.go
+++ json.go
@@ -10,9 +10,10 @@ import (
 //复杂数据结构间的转换函数
 func JsonToObject(input interface{}, output interface{}) error {
 	//首先要判断两个参数的类型，只有指针才可以进行以下操作，否则直接返回错误
-	ptr1 := reflect.TypeOf(input)
-	ptr2 := reflect.TypeOf(output)
-	if ptr1.Kind() != reflect.Ptr || ptr2.Kind() != reflect.Ptr {
+	//ptr1 := reflect.TypeOf(input).//普通方式获取类型，判断时需要使用.Kind()
+	//ptr2 := reflect.TypeOf(output)//普通方式获取类型，判断时需要使用.Kind()
+	//用封装函数获取类型
+	if GetType(input) != reflect.Ptr || GetType(output) != reflect.Ptr {
 		return errors.New("参数类型不是指针，请取变量地址进行传参")
 	}
 
